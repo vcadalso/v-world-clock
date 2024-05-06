f7990a1921b44e9313f89d977190ff7e2b2f8f33
vcadalso <vanessa.cadalso@gmail.com>
2024-05-06 13:25:44 -0400

    Created functional dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -18,5 +18,25 @@ sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
 sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>] A [</small>]");
 }
 
+function updateCity(event){
+    let cityTimeZone = event.target.value;
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML =`
+    <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss")}<small> ${cityTime.format("A")}</small></div>
+        </div>
+       `;
+
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
