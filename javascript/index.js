e66d22460d0a33f17ba001d2ad51309e36fb0b64
vcadalso <vanessa.cadalso@gmail.com>
2024-05-07 10:38:13 -0400

    Added homepage link

--- javascript/index.js
+++ javascript/index.js
@@ -34,6 +34,7 @@ function updateCity(event){
           </div>
           <div class="time">${cityTime.format("h:mm:ss")}<small> ${cityTime.format("A")}</small></div>
         </div>
+        <a href="/"> Back to homepage</a>
        `;
 
 }
