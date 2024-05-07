3feebe2024a32530b72397f2bd9764dbbd3e8f54
vcadalso <vanessa.cadalso@gmail.com>
2024-05-07 10:31:30 -0400

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -20,6 +20,9 @@ sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>] A [</small>]"
 
 function updateCity(event){
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
     let cityTime = moment().tz(cityTimeZone);
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let citiesElement = document.querySelector("#cities");
